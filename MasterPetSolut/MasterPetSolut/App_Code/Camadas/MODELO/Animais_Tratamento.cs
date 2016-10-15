9b18208ef3f1e916b22f5a474091cf4407ad2db7
Pedro Foganholi <pedro.foganholi3@hotmail.com>
2016-10-15 20:46:05 -0300

    OI

---
+++ MasterPetSolut/MasterPetSolut/App_Code/Camadas/MODELO/Animais_Tratamento.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MasterPetSolut.App_Code.Camadas.MODEL
+{
+    public class Animais_Tratamento
+    {
+        public int idAnimais { get; set; }
+        public int idTratamento { get; set; }
+    }
+}
---
+++ MasterPetSolut/MasterPetSolut/App_Code/Camadas/MODELO/Clientes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MasterPetSolut.Camadas.MODEL
+{
+    public class Clientes
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public DateTime aniversario { get; set; }
+        public string endereco { get; set; }
+        public int idCidade { get; set; }
+    }
+}
