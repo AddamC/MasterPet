df1620b1b80532308d50db87499e78842f4f2145
AddamCauê <AddamCau@ADDAM>
2016-10-15 06:01:31 -0300

    Introdução

---
+++ MasterPetSolution/MasterPetSolution/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MasterPetSolution.Startup))]
+namespace MasterPetSolution
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
