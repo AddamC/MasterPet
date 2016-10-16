31a86bdc02e629150fb22fd9e993b4edd2efeb86
AddamCauê <AddamCau@ADDAM>
2016-10-15 23:10:51 -0300

    DAL e DALL 2

--- MasterPetSolut/MasterPetSolut/App_Code/Camadas/DAL/Animais.cs
+++ MasterPetSolut/MasterPetSolut/App_Code/Camadas/DAL/Animais.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -26,16 +27,21 @@ namespace MasterPetSolut.App_Code.Camadas.DAL
                     animais.id = Convert.ToInt32(reader["id"]);
                     animais.nome = reader["nome"].ToString();
                     animais.raca = reader["raca"].ToString();
-                    animais.especie = reader[""]
-                    lstCidade.Add(cidade);
+                    animais.especie = reader["especie"].ToString();
+                    animais.cor = reader["cor"].ToString();
+                    animais.sexo = Convert.ToChar(reader["sexo"]);
+                    animais.nascimento = Convert.ToDateTime(reader["nascimento"]);
+                    lstAnimais.Add(animais);
                 }
             }
             catch
             {
-
-                throw;
+                Console.WriteLine("Deu erro na seleção de Animais");
             }
-
+            finally
+            {
+                conexao.Close();
+            }
             return lstAnimais;
         }
 
@@ -59,6 +65,56 @@ namespace MasterPetSolut.App_Code.Camadas.DAL
             catch
             {
                 Console.WriteLine("Deu erro na inserção de Animais...");
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        public void Update(MODEL.Animais animais) 
+        {
+            SqlConnection conexao = new SqlConnection(strCon);
+            string sql = "Update Animais set idClientes=@idClientes, nome=@nome, raca=@raca," +
+                "especie=@especie, cor=@cor, sexo=@sexo, nascimento=@nascimento where id=@id;";
+            SqlCommand cmd = new SqlCommand(sql, conexao);
+            cmd.Parameters.AddWithValue("@id", animais.id);
+            cmd.Parameters.AddWithValue("@idClientes", animais.idClientes);
+            cmd.Parameters.AddWithValue("@nome", animais.nome);
+            cmd.Parameters.AddWithValue("@raca", animais.raca);
+            cmd.Parameters.AddWithValue("@especie", animais.especie);
+            cmd.Parameters.AddWithValue("@cor", animais.cor);
+            cmd.Parameters.AddWithValue("@sexo", animais.sexo);
+            cmd.Parameters.AddWithValue("@nascimento", animais.nascimento);
+            conexao.Open();
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                Console.WriteLine("Erro na atualização de Animais");
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        public void Delete(MODEL.Animais animais)
+        {
+            SqlConnection conexao = new SqlConnection(strCon);
+            string sql = "Delete from Animais where id=@id;";
+            SqlCommand cmd = new SqlCommand(sql, conexao);
+            cmd.Parameters.AddWithValue("@id", animais.id);
+            conexao.Open();
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                Console.WriteLine("Erro na Remoção de Animais");
             }
             finally
             {
--- MasterPetSolut/MasterPetSolut/App_Code/Camadas/DAL/Clientes.cs
+++ MasterPetSolut/MasterPetSolut/App_Code/Camadas/DAL/Clientes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,33 @@ namespace MasterPetSolut.App_Code.Camadas.DAL
     public class Clientes
     {
         public string strCon = DAL.Conexao.getConexao();
+
+        public List<MODEL.Clientes> Select()
+        {
+            List<MODEL.Clientes> lstClientes = new List<MODEL.Clientes>();
+            SqlConnection conexao = new SqlConnection(strCon);
+            string sql = "Select * from Clientes";
+            SqlCommand cmd = new SqlCommand(sql, conexao);
+            SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            try
+            {
+                MODEL.Clientes clientes = new MODEL.Clientes();
+                clientes.nome = reader["nome"].ToString();
+                clientes.nascimento = Convert.ToDateTime(reader["nascimento"]);
+                clientes.endereco = reader["endereco"].ToString();
+                clientes.idCidade = Convert.ToInt32(reader["idCidade"]);
+                lstClientes.Add(clientes);
+            }
+            catch
+            {
+                Console.WriteLine("Deu erro na seleção de clientes");
+            }
+            finally
+            {
+                conexao.Close();
+            }
+            return lstClientes;
+        }
 
         public void Insert(MODEL.Clientes cliente)
         {
@@ -27,6 +55,52 @@ namespace MasterPetSolut.App_Code.Camadas.DAL
             catch
             {
                 Console.WriteLine("Deu erro na inserção de Clientes...");
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        public void Update(MODEL.Clientes clientes)
+        {
+            SqlConnection conexao = new SqlConnection(strCon);
+            string sql = "Update Clientes set idCidade=@idCidade, nome=@nome, nascimento=@nascimento, endereco=endereco where id=@id;";
+            SqlCommand cmd = new SqlCommand(sql, conexao);
+            cmd.Parameters.AddWithValue("@id", clientes.id);
+            cmd.Parameters.AddWithValue("@idCidade", clientes.idCidade);
+            cmd.Parameters.AddWithValue("@nome", clientes.nome);
+            cmd.Parameters.AddWithValue("@nascimento", clientes.nascimento);
+            cmd.Parameters.AddWithValue("@endereco", clientes.endereco);
+            conexao.Open();
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                Console.WriteLine("Erro na atualização de Clientes");
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+        }
+        public void Delete(MODEL.Clientes clientes)
+        {
+            SqlConnection conexao = new SqlConnection(strCon);
+            string sql = "Delete from Clientes where id=@id;";
+            SqlCommand cmd = new SqlCommand(sql, conexao);
+            cmd.Parameters.AddWithValue("@id", clientes.id);
+            conexao.Open();
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                Console.WriteLine("Erro na Remoção de Clientes");
             }
             finally
             {
