1e35f370e76620c923aabdc8adb5653b2d296980
AddamCauê <AddamCau@ADDAM>
2016-10-15 21:17:41 -0300

    conexao

--- MasterPetSolut/MasterPetSolut/App_Code/Camadas/DAL/Clientes.cs
+++ MasterPetSolut/MasterPetSolut/App_Code/Camadas/DAL/Clientes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,27 @@ namespace MasterPetSolut.App_Code.Camadas.DAL
 {
     public class Clientes
     {
-
-        MODEL.Clientes
+        public string strCon = DAL.Conexao.getConexao();
+        public void Insert(MODEL.Cidades cidade)
+        {
+            SqlConnection conexao = new SqlConnection(strCon);
+            string sql = "Insert into Cidades values (@nome, @uf)";
+            SqlCommand cmd = new SqlCommand(sql, conexao);
+            cmd.Parameters.AddWithValue("@nome", cidade.nome);
+            cmd.Parameters.AddWithValue("@uf", cidade.uf);
+            conexao.Open();
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                Console.WriteLine("Deu erro na inserção de Cidades...");
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
     }
 }
